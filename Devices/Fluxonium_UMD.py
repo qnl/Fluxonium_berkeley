d0b23cafdedc25d2b448682a1038c22a7cdd2ac0
qubearfarmer <nbaolong89@gmail.com>
2021-01-05 00:18:12 -0800

    update notebooks

--- Devices/Fluxonium_UMD.py
+++ Devices/Fluxonium_UMD.py
@@ -1,48 +1,41 @@
-devices = [
-{'name':'Fluxonium #11',
-     'E_J':3.37,
-     'E_L':1.283,
-     'E_C':0.845,
-     'junc_area':130*160,
-     'array_junc_num':100,
-     'array_junc_area':2*0.4
-     },
+def devices():
+    list_of_devices = [
+        {'name': 'Fluxonium #11',
+         'E_J': 3.37,
+         'E_L': 1.283,
+         'E_C': 0.845,
+         'junc_area': 130 * 160,
+         'array_junc_num': 100,
+         'array_junc_area': 2 * 0.4
+         },
 
-    {'name':'Fluxonium #12',
-     'E_J':1.6,
-     'E_L':0.5,
-     'E_C':0.86,
-     'junc_area':130*160,
-     'array_junc_num':100,
-     'array_junc_area':2*0.4
-     },
+        {'name': 'Fluxonium #12',
+         'E_J': 1.6,
+         'E_L': 0.5,
+         'E_C': 0.86,
+         'junc_area': 130 * 160,
+         'array_junc_num': 100,
+         'array_junc_area': 2 * 0.4
+         },
 
-{'name':'Fluxonium #13',
-     'E_J':3,
-     'E_L':1,
-     'E_C':0.84,
-     'junc_area':130*160,
-     'array_junc_num':100,
-     'array_junc_area':2*0.4
-     },
+        {'name': 'Fluxonium #13',
+         'E_J': 3,
+         'E_L': 1,
+         'E_C': 0.84,
+         'junc_area': 130 * 160,
+         'array_junc_num': 100,
+         'array_junc_area': 2 * 0.4
+         },
 
-{'name':'Fluxonium #14',
-     'E_J':6.4,
-     'E_L':1.6,
-     'E_C':0.83,
-     'junc_area':160*180,
-     'array_junc_num':100,
-     'array_junc_area':2*0.4
-     }
-]
-
-device1 = {'name':'Fluxonium #14',
-     'E_J':6.4,
-     'E_L':1.6,
-     'E_C':0.83,
-     'junc_area':160*180,
-     'array_junc_num':100,
-     'array_junc_area':2*0.4
-     }
+        {'name': 'Fluxonium #14',
+         'E_J': 6.4,
+         'E_L': 1.6,
+         'E_C': 0.83,
+         'junc_area': 160 * 180,
+         'array_junc_num': 100,
+         'array_junc_area': 2 * 0.4
+         }
+    ]
+    return list_of_devices
 
 
